f24c56d9b90bb9ead8ee252ab05b116eaff77446
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-10-06 10:50:02 -0500

    login_page responsive

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:deezer_app/utils/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -5,12 +6,14 @@ import 'package:flutter/services.dart';
 import 'package:deezer_app/pages/login/widgets/welcome.dart';
 import 'package:deezer_app/pages/login/widgets/login_form.dart';
 
+import 'package:after_layout/after_layout.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with AfterLayoutMixin {
   @override
   void initState() {
     super.initState();
@@ -21,15 +24,62 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   @override
+  void afterFirstLayout(BuildContext context) {
+    final bool isTable = MediaQuery.of(context).size.shortestSide >= 600;
+    if (!isTable) {
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Responsive responsive = Responsive.of(context);
+
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[Welcome(), LoginForm()],
-        ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.white,
+            child: OrientationBuilder(
+              builder: (_, Orientation orientation) {
+                if (orientation == Orientation.portrait) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      height: responsive.height,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[Welcome(), LoginForm()],
+                      ),
+                    ),
+                  );
+                } else {
+                  return Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: SingleChildScrollView(
+                            physics: NeverScrollableScrollPhysics(),
+                            child: Container(
+                              padding: EdgeInsets.only(left: 20),
+                              height: responsive.height,
+                              child: Center(
+                                child: Welcome(),
+                              ),
+                            )),
+                      ),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Container(child: Center(child: LoginForm())),
+                        ),
+                      )
+                    ],
+                  );
+                }
+              },
+            )),
       ),
     );
   }
--- lib/pages/login/widgets/login_form.dart
+++ lib/pages/login/widgets/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:deezer_app/utils/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -9,65 +10,84 @@ import 'package:deezer_app/widgets/circle_button.dart';
 class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 280,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          InputTextLogin(
-            iconPath: 'assets/pages/login/icons/email.svg',
-            placeholder: 'Email Address',
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          InputTextLogin(
-            iconPath: 'assets/pages/login/icons/key.svg',
-            placeholder: 'Password',
-          ),
-          Container(
-            width: double.infinity,
-            alignment: Alignment.centerRight,
-            child: CupertinoButton(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child:
-                  Text("Forgot password", style: TextStyle(fontFamily: 'Sans')),
+    final Responsive responsive = Responsive.of(context);
+    return SafeArea(
+      top: false,
+      child: Container(
+        width: 280,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            InputTextLogin(
+              iconPath: 'assets/pages/login/icons/email.svg',
+              placeholder: 'Email Address',
+            ),
+            SizedBox(
+              height: responsive.ip(2),
+            ),
+            InputTextLogin(
+              iconPath: 'assets/pages/login/icons/key.svg',
+              placeholder: 'Password',
+            ),
+            Container(
+              width: double.infinity,
+              alignment: Alignment.centerRight,
+              child: CupertinoButton(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: Text("Forgot password",
+                    style: TextStyle(fontFamily: 'Sans')),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(
+              height: responsive.ip(2),
+            ),
+            RoundedButton(
+              label: "Sign In",
               onPressed: () {},
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          RoundedButton(
-            label: "Sign In",
-            onPressed: () {},
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text("Or continue with"),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleButton(
-                iconPath: "assets/pages/login/icons/facebook.svg",
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              CircleButton(
-                iconPath: "assets/pages/login/icons/google.svg",
-                backgroundColor: Color(0xffD50000),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 100,
-          ),
-        ],
+            SizedBox(
+              height: responsive.ip(3),
+            ),
+            Text("Or continue with"),
+            SizedBox(
+              height: responsive.ip(1),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleButton(
+                  iconPath: "assets/pages/login/icons/facebook.svg",
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                CircleButton(
+                  iconPath: "assets/pages/login/icons/google.svg",
+                  backgroundColor: Color(0xffD50000),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: responsive.ip(2.7),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Don't have an account?"),
+                CupertinoButton(
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontFamily: 'Sans'),
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
