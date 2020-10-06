cd22246586ac7ac03358a6420c2e24114f226777
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-10-06 11:22:04 -0500

    firebase installed in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 // Pages
 import 'package:deezer_app/pages/login/login_page.dart';
 import 'package:deezer_app/pages/home/home_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  // await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
+  Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    // statusBarColor: Colors.transparent,
-    // ));
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Sans'),
-        title: 'Dezzer App',
-        home: LoginPage());
+    return FutureBuilder(
+      future: _initialization,
+      builder: (_, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Firebase erroe"),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Sans'),
+              title: 'Dezzer App',
+              home: LoginPage());
+        }
+
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
